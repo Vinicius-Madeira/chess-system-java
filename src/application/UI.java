1a525d68743cf47131a7b00a3336f65ce37519eb
Vinicius-Madeira <viniciussmadeira@gmail.com>
2022-12-09 20:07:49 -0300

    Handling exceptions and clearing screen

--- src/application/UI.java
+++ src/application/UI.java
@@ -30,6 +30,11 @@ public class UI {
     public static final String ANSI_CYAN_BACKGROUND = "\u001B[46m";
     public static final String ANSI_WHITE_BACKGROUND = "\u001B[47m";
 
+    // https://stackoverflow.com/questions/2979383/java-clear-the-console
+    public static void clearScreen() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
 
     public static ChessPosition readChessPosition(Scanner sc) {
         try {
@@ -49,7 +54,7 @@ public class UI {
             }
             System.out.println();
         }
-        System.out.print("  A B C D E F G H");
+        System.out.print("  a b c d e f g h");
     }
 
     private static void printPiece(ChessPiece piece) {
